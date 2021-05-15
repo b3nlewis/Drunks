d39e15752b39b48779c4c490ae960701f05b12e9
b3nlewis <72766300+b3nlewis@users.noreply.github.com>
2021-05-15 15:04:32 +0100

    Update
    First figure does not close properly

--- Drunks.py
+++ Drunks.py
@@ -6,186 +6,373 @@ This is a temporary script file.
 """
 
 '''Import Statements'''
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.figure import Figure
 import random
 import numpy
 import matplotlib.pyplot as plt
-import csv
-import drunkFramework
-import time
-import pandas as pd
-import seaborn as sns
+import csv # to read files
+import drunkFrameworkadvance
+import time #for testing
+import pandas as pd #for dataframes
+import seaborn as sns #for heatmaps, looks better than plt
+import sys
+import argparse #used for commandline interface.
+import tkinter
 
-'''Step 1'''
+    #Commandline interface
+# parser = argparse.ArgumentParser(description=
+# 'Run a Random Walk Agent Based Model where drunk people are simulated.')
+
+# parser.add_argument('-s', '--seed', type=int, metavar = '', help='Assign a seed to the model')
+# parser.add_argument('-e', '--exportName', type=str, metavar = '', help='Assign a name for the txt export file')
+#args = parser.parse_args()
+
+print('Step 1: Initialisaing Model')
+'''Step 1: Initialisation'''
 xAxis = 300
+print('X Axis:', xAxis)
 yAxis = 300
+print('Y Axis:', yAxis)
 environment = []
 pubList = []
 houseList = []
 drunks = []
-random.seed()
-
-
-'''Step 2'''
-#environment = []
-#for i in range(xAxis):				# A list of rows
-#    rowlist = []      
-#    for j in range(yAxis):	
-#        i = 0		# A list of value
-#        rowlist.append(i)   # Append values to rowlist
-#    environment.append(rowlist) # Append rowlist to environment 
-
-#print(environment)
-
-file = open('environment.txt', newline='') 
-reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
-for row in reader:				# A list of rows
-    rowlist = []                # An empty list
-    for value in row:			# A list of value
-        rowlist.append(int(value))   # Append values to rowlist
-    environment.append(rowlist) # Append rowlist to environment 
-file.close()
-
-plt.imshow(environment)
-plt.show
-
-#file = open('environment.txt', 'w', newline='') 
-#writer = csv.writer(file, delimiter=',')
-#for row in environment:		
-#	writer.writerow(row)		# List of values.
-#file.close()
-
-'''Step 3: Get coordinates of all pubs in environment'''
-
-x = 0
-for row in (environment):
-    rowlist = []
-    x += 1
-    y = 0
-    for j in row:
-        y +=1
-        if j == 1:
-            pubList.append([j, y, x])
-            
-print(pubList) # A list of lists containing pub x-coordinate and y-coordinate.
-    
-'''Step 4: Get coordinates of all house in environment'''
-
-x = 0
-for row in (environment):
-    rowlist = []
-    x += 1
-    y = 0
-    for i in row:
-        y +=1
-        if i != 1 and i != 0:
-            houseList.append([i, y, x])
-            
-print(houseList) # A list of lists containing pub x-coordinate and y-coordinate.
-
-'''Assigning drunks to a pub (starting location) and a house (ending location).'''
-random.shuffle(pubList)#randomises where drunks start
-random.shuffle(houseList)#randomises where drunks finish
-for i in range(25): #number of drunks
-    n = random.randint(0,8)
-    pubX, pubY = pubList[n][1], pubList[n][2]
-    houseX, houseY = houseList[i][1], houseList[i][2]
-    drunks.append(drunkFramework.drunk(pubX, pubY, houseX, houseY))
-
-#Testing                  
-print(drunks)
-for drunk in(drunks):
-    drunk.starting_location()
-    drunk.ending_location()
-#################################
-
-'''Moving drunks'''
-drunk_num = 0
-for i in (drunks):
-    start = time.process_time()
-    #num_of_steps = 0
-    drunk_num += 1
-    while i.is_home() == False:
-        i.move()
-       # num_of_steps += 1
-    else:
-        end = time.process_time()
-        print("Drunk", drunk_num,"finished in", i.distance, "steps and", end - start, "seconds.")
-        #print(i.is_home())
-
-##################################
- 
-'''Creating table to store cell counts'''
-print('Starting Table')
-data = []
-for row in range(0,300):
-    for col in range(0, 300):
-        data.append([row,col])
-data = pd.DataFrame(data)
-data.columns = ['x', 'y']
-print(data)
-print('Finished Tables')
-##################################
-
-'''creating a dictionary which counts the number of occurences of
-a cell for all drunks'''
-
-count = {}
-for i in drunks:
-    df_route = i.get_route()
-    print(df_route)
-    column = df_route[0]
-    for entry in column:
-        if entry in count.keys():
-            count[entry] += 1
-        else:
-            count[entry] = 1
-#print(count)
-
-
-'''converts dictrionary into a dataframe'''    
-cell_count = pd.DataFrame(list(count.items()), columns = ['Coords', 'Amount'])
-cell_count['x'], cell_count['y'] = zip(*cell_count.Coords)
-cell_count.pop('Coords')
-
-print(cell_count)
-
-'''merges dataframe of all cells with cell occurences'''            
-merge_data = pd.merge(data, cell_count)
-
-'''Sort df into rows'''
-sorted_data = merge_data.sort_values(['y', 'x'], ascending=[True, True])
-print(sorted_data.columns)
-print(sorted_data)
- 
-'''converts into a list'''
-list_of_amount = sorted_data['Amount'].values.tolist()
-print(list_of_amount)
-
-'''converts into list of list'''
-i = 0
 new_list = []
-while i <len(list_of_amount):
-    new_list.append(list_of_amount[i : i + 300])
-    i += 300
+random.seed(10)
+print('Random Seed:', random.random())
+is_running=False
+
+
+
+print('Step 2: Defining Functions')
+'''Step 2: Define Functions'''
+def open_file(input_file, output_file, var_type):
+    '''
     
-print(new_list)
+    Parameters
+    ----------
+    input_file : String
+        DESCRIPTION.
+    output_file : variable
+        DESCRIPTION.
+    var_type : TYPE
+        DESCRIPTION.
 
-'''creates map'''
-ax = sns.heatmap(new_list)
+    Returns
+    -------
+    Returns the input file as a List
 
-'''export data'''
-file_finished = open('export_walks.txt', 'w', newline='') 
-writer = csv.writer(file_finished, delimiter=',')
-for row in new_list:		
-	writer.writerow(row)		# List of values.
-file.close()
+    '''
+    
+    file = open(input_file, newline='') 
+    reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
+    for row in reader:				# A list of rows
+        rowlist = []                # An empty list
+        for value in row:			# A list of value
+            rowlist.append(var_type(value))   # Append values to rowlist
+        output_file.append(rowlist) # Append rowlist to environment 
+    file.close()
+    
+def write_file(input_file, output_file):
+    '''
+    
+    Parameters
+    ----------
+    input_file : TYPE
+        DESCRIPTION.
+    output_file : TYPE
+        DESCRIPTION.
+  
+    Returns
+    -------
+    None.
 
-# #################################
+    '''
+    file_finished = open(output_file, 'w', newline='') 
+    writer = csv.writer(file_finished, delimiter=',')
+    for row in input_file:		
+    	writer.writerow(row)		# List of values.
+    file_finished.close()
+
+def main():
+    '''
     
 
-# # for i in range(25): #Number of houses
-# #      plt.scatter(houseList[i][1], houseList[i][2], c='Red')
-# # for j in range(9): #Number of pubs
-# #      plt.scatter(pubList[j][1], pubList[j][2], c='Green')
-# # plt.show()
+    Returns
+    -------
+    None.
 
+    '''
+    print('Step 3: Creating Base Environment')
+    '''Step 3: Create Environment'''
+    '''creates a 300 by 300 grid'''
+    #for i in range(xAxis):				# A list of rows
+    #    rowlist = []      
+    #    for j in range(yAxis):	
+    #        i = 0		# A list of value
+    #        rowlist.append(i)   # Append values to rowlist
+    #    environment.append(rowlist) # Append rowlist to environment 
+    
+    #print(environment)
+    
+    '''Opens the environment file to read where pubs house are located
+    Inserts each row of file into a list of lists'''
+    
+    open_file('environment.txt', environment, int) 
+    
+    print('Step 4: Locating Pubs and Houses')
+    '''Step 4: Get coordinates of all pubs in environment'''
+    
+    y = 0 #starting value
+    for row in (environment): #goes over each row of list at a time
+        y += 1 #finds position on y axis.
+        x = 0 #starting value
+        for i in row: #goes over each value in row
+            x +=1 #finds position on y axis.
+            if i == 1: #Pub values are equal to 1.
+                pubList.append([i, x, y])# adds x and y values of each pub to a list.
+    
+    print('Pub List')           
+    print(pubList) # A list of lists containing pub x-coordinate and y-coordinate.
+     
+    
+    '''Step 4: Get coordinates of all house in environment'''
+    
+    y = 0 #starting value
+    for row in (environment): #goes over each row of list at a time
+        y += 1 #finds position on y axis.
+        x = 0 #starting value
+        for i in row: #goes over each value in row
+            x +=1 #finds position on x axis. 
+            if i != 1 and i != 0:
+                houseList.append([i, x, y])
+                
+    print('House List')             
+    print(houseList) # A list of lists containing pub x-coordinate and y-coordinate.
+    
+    
+def displayData():
+    '''
+    
+
+    Returns
+    -------
+    None.
+
+    '''
+    print('Step 5: Displaying Environment, Pubs and Houses')
+    '''show pubs and house'''
+  
+    # the figure that will contain the plot
+    fig = plt.figure(figsize=(8,8))
+  
+
+    plot1 = fig.add_subplot(111)
+    plot1 = plt.imshow(environment)
+    for i in range(25): #Number of houses
+        plot1 = plt.scatter(houseList[i][1], houseList[i][2], c='Red')
+    for j in range(9): #Number of pubs
+        plot1 = plt.scatter(pubList[j][1], pubList[j][2], c='Green', label='Pubs')
+    # containing the Matplotlib figure
+    canvas =  matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master = root)  
+    
+    canvas.draw()
+  
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+
+    plt.close()
+
+    
+    
+    
+    
+def runModel():
+    '''
+    
+
+    Returns
+    -------
+    None.
+
+    '''
+    print('Step 6: Assigning Start and End Locations')    
+    '''Step 6: Assigning drunks to start and end locations'''
+    '''Assigning drunks to a pub (starting location) and a house (ending location).'''
+    random.shuffle(pubList)#randomises where drunks start
+    random.shuffle(houseList)#randomises where drunks finish
+    for i in range(25): #number of drunks
+        n = random.randint(0,8)
+        pubX, pubY = pubList[n][0], pubList[n][1]
+        houseX, houseY = houseList[i][1], houseList[i][2]
+        drunks.append(drunkFrameworkadvance.drunk(pubX, pubY, houseX, houseY))
+    
+    #Testing                  
+    print(drunks)
+    for i in(drunks):
+        print("Drunk ", i)
+        i.starting_location()
+        i.ending_location()
+        
+    #################################
+    
+    print('Step 7: Moving Drunks')
+    '''Step 7: Moving Drunks'''
+    drunk_num = 0
+    for i in (drunks):
+        start = time.process_time()
+        drunk_num += 1
+        while i.is_home() == False:
+            i.move()
+        else:
+            end = time.process_time()
+            print("Drunk", drunk_num,"finished in", i.distance, "steps and", end - start, "seconds.")
+            #print(i.is_home())
+    
+    print('Step 8: Storing and Sorting Drunk Results')
+    '''Step 8: Storing and sorting drunk movements'''
+    
+    '''Creating table to store cell counts'''
+    print('Starting Table')
+    data = []
+    for x in range(0,300):
+        for y in range(0, 300):
+            data.append([x, y])
+    data = pd.DataFrame(data)
+    data.columns = ['x', 'y']
+    print(data)
+    print('Finished Tables')
+
+    
+    '''Creating a dictionary which counts the number of occurences of
+    a cell for all drunks'''
+    
+    count = {}
+    for i in drunks:
+        df_route = i.get_route()
+        print(df_route)
+        column = df_route[0]
+        for entry in column:
+            if entry in count.keys():
+                count[entry] += 1
+            else:
+                count[entry] = 1
+    #print(count)
+    
+    
+    '''converts dictrionary into a dataframe'''    
+    cell_count = pd.DataFrame(list(count.items()), columns = ['Coords', 'Amount'])
+    cell_count['x'], cell_count['y'] = zip(*cell_count.Coords)
+    cell_count.pop('Coords')
+    
+    print(cell_count)
+    
+    '''merges dataframe of all cells with cell occurences'''            
+    merge_data = pd.merge(data, cell_count)
+    
+    '''Sort df into rows'''
+    sorted_data = merge_data.sort_values(['y', 'x'], ascending=[True, True])
+    print(sorted_data.columns)
+    print(sorted_data)
+     
+    '''converts into a list'''
+    list_of_amount = sorted_data['Amount'].values.tolist()
+    print(list_of_amount)
+    
+    '''converts into list of list'''
+    global new_list
+    i = 0
+    new_list = []
+    while i <len(list_of_amount):
+        new_list.append(list_of_amount[i : i + 300])
+        i += 300
+        
+    print(new_list)
+    
+    print('Step 9: Creating a Map of Cell Densities')
+    '''Step 9: Creating a Map'''
+    canvas.get_tk_widget('canvas').destroy()
+    fig = plt.figure(figsize=(8,8))
+    
+  
+
+    plot1 = fig.add_subplot(111)
+    plot1 = sns.heatmap(new_list)
+    plot1.set_title("Random Walk Density Map", fontsize=18)
+    plt.close()
+    canvas =  matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master = root)  
+    
+    canvas.draw()
+  
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+
+  
+    
+def exportMap():
+    '''
+    
+
+    Returns
+    -------
+    None.
+
+    '''
+    plt.savefig('walk_heatmap.png')
+    plt.close()
+    print('Map Exported')
+    
+
+def exportData():
+    '''
+    
+
+    Returns
+    -------
+    None.
+
+    '''
+    '''Step 10: Exporting Data'''
+    write_file(new_list, 'walk_data_export.txt')
+
+    print('Data Exported')
+
+    
+def exiting():
+    '''
+    
+
+    Returns
+    -------
+    None.
+
+    '''
+    #need to clear map
+    root.quit()
+    root.destroy()
+    print('Model Closed')
+    
+
+        
+
+
+'''Creating GUI'''
+root = tkinter.Tk()
+root.wm_title("Model")
+root.geometry('800x800')
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label="Model", menu=model_menu)
+model_menu.add_command(label="Display Pubs and Houses", command=displayData)
+model_menu.add_command(label="Run model", command=runModel)
+model_menu.add_command(label="Export Data", command=exportData)
+model_menu.add_command(label="Export Map", command=exportMap)
+model_menu.add_command(label="Exit model", command=exiting) 
+
+if __name__ == '__main__':
+    main()
+root.protocol('WM_DELETE_WINDOW', exiting)#When the GUI window is closed the exiting function is run.
+tkinter.mainloop()#GUI is constantly checking if it has been clicked.
--- drunkFrameworkadvance.py
+++ drunkFrameworkadvance.py
@@ -7,21 +7,17 @@ Created on Sun May  9 19:05:38 2021
 
 import random
 import pandas as pd
-random.seed(10)
+random.seed()
 
 class drunk():
     def __init__(self, pubX, pubY, houseX, houseY):
         self.x = pubX #initial starting location
         self.y = pubY
         self.start = (pubX, pubY)
-        self.startX = pubX
-        self.startY = pubY
         self.end = (houseX, houseY)#house
         self.route = []
-        self.stuck = []
         self.distance = 0
         self.position = []
-        self.j = 0
         
     def starting_location(self):
         print(self.start)
@@ -32,62 +28,19 @@ class drunk():
         return(self.end)
     
     def move(self):
+        
+        #if self.check_cell() == False:
         (dx, dy) = random.choice([(0, 1), (1, 0), (0, -1), (-1, 0)]) # N, E, S, W
-        x = dx + self.x
-        y = dy + self.y
-        if ([(x, y)]) not in self.stuck:
-            i = 0
-            while i <= 3:
-                if ([(x, y)]) not in self.route:
-                    #print([(x, y)])
-                    self.x = (self.x + dx) % 300
-                    self.y = (self.y + dy) % 300
-                    self.route.append([(self.x, self.y)]) #So agent can see if they have been here before.
-                    self.distance = self.distance + 1
-                    i += 1
-                else:
-                    self.x = self.startX
-                    self.y = self.startY
-                    self.stuck = self.route[-1]
-                    self.route = []
+        self.x = (self.x + dx) % 300
+        self.y = (self.y + dy) % 300
+        self.route.append([(self.x, self.y)]) #So agent can see if they have been here before.
+        self.distance = self.distance + 1
+        # else:
+        #     self.x = self.position[0]
+        #     self.y = self.position[1]
+        #     self.distance = self.distance + 1
+
             
-            
-        
-        
-            # moves = [(0, 1), (1, 0), (0, -1), (-1, 0)] # N, E, S, W
-            # updated_moves = random.sample(moves, 4)
-            # #print(updated_moves)
-            # x = self.x
-            # y = self.y
-            # i = 0
-            # for i in range(0, 4, 1):
-            #     #print("loop", i)
-            #     (dx, dy) = updated_moves[i] 
-            #     x = (x + dx) % 300
-            #     y = (y + dx) % 300
-            #     i += 1
-            #     if ([(x, y)]) not in self.route:
-            #         self.x = (x + dx) % 300
-            #         self.y = (y + dx) % 300
-            #         self.route.append([(self.x, self.y)]) #So agent can see if they have been here before.
-            #         self.distance = self.distance + 1
-            #         #self.j += 1
-            #         #print("2")
-            #         #print("loop", i)
-            #         return
-            #     else:
-            #         pass
-            # else:
-            #     (dx, dy) = random.choice([(0, 1), (1, 0), (0, -1), (-1, 0)]) # N, E, S, W
-            #     self.x = (self.x + dx) % 300
-            #     self.y = (self.y + dy) % 300
-            #     self.route.append([(self.x, self.y)]) #So agent can see if they have been here before.
-            #     self.distance = self.distance + 1
-            #     #self.j += 1
-            #     #print("3")
-            # #print(self.j)
-                
-                    
     def is_home(self):
         if (self.x, self.y) != self.end:
             #print("False")
@@ -99,18 +52,6 @@ class drunk():
     def get_route(self):
         df = pd.DataFrame(self.route)
         return(df)
-              
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     # def check_cell(self):
     #     check = [[0, 1], [1, 0], [0, -1], [-1, 0]]
@@ -154,4 +95,6 @@ class drunk():
             #     randVal = random.choice(check)
             #     try_pos = [sum(i) for i in zip(position, randVal)]
             #     self.try_pos = try_pos
-            #     return self.try_pos                          
+            #     return self.try_pos
+                    
+                
